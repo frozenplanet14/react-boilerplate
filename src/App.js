abbe8b2d4d3dc4ffe16ae48acdc59a3c09c1f257
Suman Jha <suman.jha@fiserv.com>
2020-09-04 22:18:52 -0700

    hot reload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
   state = {
@@ -17,4 +18,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
